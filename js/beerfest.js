dca34ab909a39e4436ee0ff12feef682578053a2
braican <nick.braica@gmail.com>
2015-01-29 17:31:52 -0500

    ui updates, and an updated data list

--- js/beerfest.js
+++ js/beerfest.js
@@ -4,170 +4,235 @@
 //
 (function(BEERFEST, $){
 
-	BEERFEST.name = "ebf-2015";
+    BEERFEST.name = "ebf-2015";
 
-	
 
-	/* --------------------------------------------
-	 * --rendering
-	 * -------------------------------------------- */
-
-	function renderBeers(data){
-
-		$.each(data, function(brewery, breweryData){
-			var breweryHtml = '<li data-brewery="' + brewery + '"><span class="breweryname">' + brewery + '</span><ul class="beers">';
-
-			$.each(breweryData.beers, function(index, beerObj){
-				var beer = beerObj.name,
-					rating = $.cookie(beer),
-					possibleRatings = [5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1, .5];
-					input = rating ? '<input type="checkbox" checked>' : '<input type="checkbox">',
-					checkedClass = rating ? ' class="checked"' : '';
-				breweryHtml +=  '<li data-beer="' + beer + '"' + checkedClass + '>' + input + '<div class="name-and-rating"><span class="beername">' + beer + '</span>' +
-									'<select>';
-
-				$.each(possibleRatings, function(i, r){
-					var v = rating == r ? '<option value="' + r + '" selected>' + r + '</option>' : '<option value="' + r + '">' + r + '</option>';
-					breweryHtml += v;
-				});
-
-				breweryHtml +=	'</select></div></li>';
-			});
-			breweryHtml += '</ul></li>';
-			$('#beerlist').append(breweryHtml);
-		});	
-	}
+    /* --------------------------------------------
+     * --util
+     * -------------------------------------------- */
+    
+    /**
+     * ajaxError
+     *
+     * console some stuff if we error out
+     */
+    function ajaxError( jqxhr, textStatus, error ) {
+        console.log( "Request Failed: " + error );
+        console.log(textStatus);
+    }
+    
 
 
 
-	/* --------------------------------------------
-	 * --UX
-	 * -------------------------------------------- */
+    /* --------------------------------------------
+     * --rendering
+     * -------------------------------------------- */
 
-	/**
-	 * scrollToLetter
-	 *
-	 * scroll the page to the selected letter
-	 * @param event: the object from the click
-	 */
-	function scrollToLetter(event){
-		event.preventDefault();
-		var letter = $(this).attr('id');
+    /**
+     * renderBeers
+     *
+     * render the markup for the list of beers
+     * @param data: the data to render
+     */
+    function renderBeers(data){
 
-		$('#beerlist > li').each(function(i, e){
-			var breweryname = $(e).data('brewery');
-			if(letter == breweryname.charAt(0).toLowerCase()){
-				$('body, html').animate({
-					'scrollTop': $(e).offset().top
-				});
-				return false;
-			}
-		});
-	}
+        $.each(data, function(brewery, breweryData){
+            var markup = '<li data-brewery="' + brewery + '"><span class="breweryname">' + brewery + '</span><ul class="beers">';
 
+            $.each(breweryData.beers, function(index, beerObj){
+                var beer         = beerObj.name,
+                    rating       = $.cookie(beer),
+                    input        = rating ? '<input type="checkbox" checked>' : '<input type="checkbox">',
+                    checkedClass = rating ? ' class="checked"' : '';
 
-	/**
-	 * deleteAllCookies 
-	 * 
-	 * remove all the cookies
-	 */
-	function deleteAllCookies() {
-	    var cookies = document.cookie.split(";");
-
-	    for (var i = 0; i < cookies.length; i++) {
-	    	var cookie = cookies[i];
-	    	var eqPos = cookie.indexOf("=");
-	    	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-	    	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-	    }
-	}
-
-	/**
-	 * clearAllData 
-	 * 
-	 * clear all markings and remove all the cookies
-	 * @param event: object from the click
-	 */
-	function clearAllData(event){
-		event.preventDefault();
-		deleteAllCookies();
-		$('input[type="checkbox"]').removeProp('checked');
-		$('li li').removeClass('checked');		
-	}
+                markup += '<li data-beer="' + beer + '"' + checkedClass + '>' +
+                            '<div class="beer-util">' +
+                                '<div class="beer-had">' + input + '</div>' +
+                                '<div class="beer-rating">' + getRatingDropdownMarkup(rating) + '</div>' +
+                            '</div>' +
+                            '<div class="beer-info">' +
+                                '<div class="beer-name">' + beer + '</div>' +
+                                '<div class="beer-extra">' +
+                                    '<div class="beer-style">' + beerObj.style + '</div>' +
+                                    '<div class="beer-abv">ABV: ' + beerObj.abv + '</div>' +
+                                    '<div class="beer-score">BA Score: ' + beerObj.ba_score + '</div>' +
+                                '</div>' +
+                            '</div>' +
+                          '</li>'
+            });
+            markup += '</ul></li>';
+            $('#beerlist').append(markup);
+        }); 
+    }
 
 
+    /**
+     * getRatingDropdownMarkup 
+     * 
+     * util function that returns the markup for the ratings dropdown
+     * @param rating: if a rating has already been chosen, select that one
+     */
+    function getRatingDropdownMarkup(rating){
+        var possibleRatings = [5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1, .5],
+            markup = '<select>';
 
-	/* --------------------------------------------
-	 * --beer interactions
-	 * -------------------------------------------- */
+        $.each(possibleRatings, function(i, r){
+            var isSelected = rating == r ? ' selected' : '';
+            markup += '<option value="' + r + '"' + isSelected + '>' + r + '</option>';
+        });
 
-	/**
-	 * saveBeerRating 
-	 * 
-	 * save the selected rating to a cookie
-	 * @param event: the object from the change event
-	 */
-	function saveBeerRating(event){
-		event.preventDefault();
-		var beername = $(this).parents('li').data('beer'),
-			rating = $(this).val();
-		
-		if($.cookie(beername)){
-			$.cookie(beername, rating);
-		}
-	}
+        markup += '</select>';
 
-
-	// =========================
-
-	BEERFEST.init = function(festName){
-		//
-		// set up cookies to expire after a year
-		//
-		$.cookie.defaults = {
-		    expires: 365
-		}
-
-		//
-		// get the beers
-		//
-		$.getJSON('data/' + festName + '.json', function(data){
-			renderBeers(data);
-			// console.log(data);
-		});
+        return markup;
+    }
 
 
 
-		$('#beerlist').on('click', '.beername', function(event){
-			event.preventDefault();
-			$(this).parent().siblings('input[type="checkbox"]').prop("checked", function(i, val){
-				return !val;
-			}).trigger('change');
-		});
+    /* --------------------------------------------
+     * --UX
+     * -------------------------------------------- */
 
-		$('#beerlist').on('change', 'input[type="checkbox"]', function(event){
-			event.preventDefault();
-			var beername = $(this).parent().data('beer'),
-				rating = $(this).parent().find('select').val();
-			if($(this).prop('checked')){
-				$.cookie(beername, rating);
-			} else {
-				$.removeCookie(beername);
-			}
-			$(this).parent().toggleClass('checked');
-		});
+    /**
+     * scrollToLetter
+     *
+     * scroll the page to the selected letter
+     * @param event: the object from the click
+     */
+    function scrollToLetter(event){
+        event.preventDefault();
+        var letter = $(this).attr('id');
 
-		$('#beerlist').on('change', 'select', saveBeerRating);
+        $('#beerlist > li').each(function(i, e){
+            var breweryname = $(e).data('brewery');
+            if(letter == breweryname.charAt(0).toLowerCase()){
+                $('body, html').animate({
+                    'scrollTop': $(e).offset().top
+                });
+                return false;
+            }
+        });
+    }
 
-		$('#clearall').on('click', clearAllData);
 
-		$('#scrollit li').on('click', scrollToLetter);
-	}
+    /**
+     * deleteAllCookies 
+     * 
+     * remove all the cookies
+     */
+    function deleteAllCookies() {
+        var cookies = document.cookie.split(";");
 
-	$(document).ready(function(){
-		// initialize the drinking!	
-		BEERFEST.init(BEERFEST.name);
-	});
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i];
+            var eqPos  = cookie.indexOf("=");
+            var name   = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        }
+    }
+
+    /**
+     * clearAllData 
+     * 
+     * clear all markings and remove all the cookies
+     * @param event: object from the click
+     */
+    function clearAllData(event){
+        event.preventDefault();
+        deleteAllCookies();
+        $('input[type="checkbox"]').removeProp('checked');
+        $('li li').removeClass('checked');      
+    }
+
+
+
+    /* --------------------------------------------
+     * --beer interactions
+     * -------------------------------------------- */
+
+    /**
+     * saveBeerRating 
+     * 
+     * save the selected rating to a cookie
+     * @param event: the object from the change event
+     */
+    function saveBeerRating(event){
+        event.preventDefault();
+        var beername = $(this).parents('li').data('beer'),
+            rating   = $(this).val();
+        
+        if($.cookie(beername)){
+            $.cookie(beername, rating);
+        }
+    }
+
+
+    /**
+     * checkOffBeer 
+     * 
+     * check or uncheck a beer
+     * @param event: obj from the click
+     */
+    function checkOffBeer(event){
+        event.preventDefault();
+        var $t       = $(this),
+            $parent  = $t.closest('li'),
+            beername = $parent.data('beer'),
+            rating   = $parent.find('select').val();
+
+        if($t.prop('checked')){
+            $.cookie(beername, rating);
+        } else {
+            $.removeCookie(beername);
+        }
+        $parent.toggleClass('checked');
+    }
+
+
+    /**
+     * triggerBeerCheck 
+     * 
+     * upon clicking anywhere on the beer name checks it off
+     * @param event: the object from the click
+     */
+    function triggerBeerCheck(event){
+        event.preventDefault();
+        $(this).closest('li').find('input[type="checkbox"]').prop("checked", function(i, val){
+            return !val;
+        }).trigger('change');                
+    }
+
+
+    // =========================
+
+    BEERFEST.init = function(festName){
+        //
+        // set up cookies to expire after a year
+        //
+        $.cookie.defaults = {
+            expires: 365
+        }
+
+        //
+        // get the beers
+        //
+        $.getJSON('data/' + festName + '.json')
+            .done(renderBeers)
+            .fail(ajaxError);
+
+
+        $('#beerlist').on('click', '.beer-name', triggerBeerCheck);
+        $('#beerlist').on('change', 'input[type="checkbox"]', checkOffBeer);
+        $('#beerlist').on('change', 'select', saveBeerRating);
+
+        $('#clearall').on('click', clearAllData);
+
+        $('#scrollit li').on('click', scrollToLetter);
+    }
+
+    $(document).ready(function(){
+        // initialize the drinking! 
+        BEERFEST.init(BEERFEST.name);
+    });
 
 })(window.BEERFEST = window.BEERFEST || {}, jQuery, undefined);
 
